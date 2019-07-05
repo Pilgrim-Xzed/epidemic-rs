6ba9f442cb53db2bff62758249ad222422b13625
Pilgrim001 <saeedbello12@gmail.com>
2019-07-06 00:47:51 +0100

    Initial dist subtree commit

---
+++ precache-manifest.008394fcd0cbf857e26085fb4e8849cd.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/epidemic-rs/img/theme/react.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/epidemic-rs/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "6f25ea9c13636b918a82",
+    "url": "/epidemic-rs/js/chunk-vendors.73c202a0.js"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/epidemic-rs/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "11a1b6b6bf5134e570d2",
+    "url": "/epidemic-rs/js/demo.1475f47d.js"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/epidemic-rs/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/epidemic-rs/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/epidemic-rs/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "aeca6659f757d3776e5d7c95e140ddf2",
+    "url": "/epidemic-rs/img/ussd.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/epidemic-rs/robots.txt"
+  },
+  {
+    "revision": "85a09f8b693d0d3144d937c637773a22",
+    "url": "/epidemic-rs/index.html"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/epidemic-rs/img/theme/vue.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/epidemic-rs/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "1009f73b2cc6d9fc00cf",
+    "url": "/epidemic-rs/js/app.b283bcef.js"
+  },
+  {
+    "revision": "ac6978cca5dd1ad29ef14141960093ef",
+    "url": "/epidemic-rs/img/realtime.png"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/epidemic-rs/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/epidemic-rs/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "fdc306fdb791ff7badfbeab764118ada",
+    "url": "/epidemic-rs/img/map.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/epidemic-rs/img/brand/white.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/epidemic-rs/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "e4ab3692df3e6f179b38f94abf9c7a76",
+    "url": "/epidemic-rs/img/icon-home-3.svg"
+  },
+  {
+    "revision": "f6a9bc69d7fac481f21524f921d7d0c8",
+    "url": "/epidemic-rs/img/dashboard.png"
+  },
+  {
+    "revision": "40891a4103e53c9cec8cd342c595b2ad",
+    "url": "/epidemic-rs/img/health.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/epidemic-rs/img/brand/green.png"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/epidemic-rs/img/brand/favicon.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/epidemic-rs/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/epidemic-rs/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/epidemic-rs/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/epidemic-rs/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "010b5142b414e8584be01d37e8515f34",
+    "url": "/epidemic-rs/css/style.css"
+  },
+  {
+    "revision": "11a1b6b6bf5134e570d2",
+    "url": "/epidemic-rs/css/demo.8f485d92.css"
+  },
+  {
+    "revision": "1009f73b2cc6d9fc00cf",
+    "url": "/epidemic-rs/css/app.73c1adba.css"
+  }
+];
